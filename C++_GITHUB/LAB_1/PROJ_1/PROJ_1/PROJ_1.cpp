3e832e86485b82dfc211f80a574b9b76048f10ea
Taras <sleeptaras@gmail.com>
2025-02-28 00:48:34 +0200

    LAB_1

---
+++ C++_GITHUB/LAB_1/PROJ_1/PROJ_1/PROJ_1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N, M;
+
+    cout << "N = ";
+    cin >> N;
+
+    cout << "M = ";
+    cin >> M;
+
+    vector<int> A(N);
+    vector<int> B(M);
+    int CMAS = N + M;
+    vector<int> C(CMAS);
+
+    int c1 = 0;
+
+    for (int i = 0; i < N; i++) {
+        cout << "A[" << i + 1 << "] = ";
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < M; i++) {
+        cout << "B[" << i + 1 << "] = ";
+        cin >> B[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (A[i] >= 0) {
+            C[c1] = A[i];
+            c1++;
+        }
+    }
+
+    for (int i = 0; i < M; i++) {
+        if (B[i] >= 0) {
+            C[c1] = B[i];
+            c1++;
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (A[i] < 0) {
+            C[c1] = A[i];
+            c1++;
+        }
+    }
+
+    for (int i = 0; i < M; i++) {
+        if (B[i] < 0) {
+            C[c1] = B[i];
+            c1++;
+        }
+    }
+
+    for (int i = 0; i < CMAS; i++) {
+        cout << "C[" << i + 1 << "] = " << C[i] << endl;
+    }
+
+    return 0;
+}
