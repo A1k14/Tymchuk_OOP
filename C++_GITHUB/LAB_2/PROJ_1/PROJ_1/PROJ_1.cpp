8641074737137f903e15121eadb05f80a3fbc0a3
Taras <sleeptaras@gmail.com>
2025-03-06 16:05:09 +0200

    proj_1 start

---
+++ C++_GITHUB/LAB_2/PROJ_1/PROJ_1/PROJ_1.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << " a, b, d, c = \n";
+	int a, b, d, c;
+    std::cin >> a >> b >> d >> c;
+
+	// (18*a + 312*d)/512 - b * 122 + c * 123
+
+	int resultat;
+
+	resultat = (a + a +  (a<< 4) + 312 * d) / 512 - b * 122 + c * 123;
+
+
+	return 0;
+
+}
