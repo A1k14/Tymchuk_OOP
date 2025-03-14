0febe760356d012c6ecf71a6eb6f44f16cdd6219
Taras <sleeptaras@gmail.com>
2025-03-14 08:10:35 +0200

    Дороблення виводу коду

--- C++_GITHUB/LAB_2/proj_2/proj_2/proj_2.cpp
+++ C++_GITHUB/LAB_2/proj_2/proj_2/proj_2.cpp
@@ -1,72 +1,68 @@
-
-/* Задано текст, який складається з 16 рядків до 4 символів у рядку. Доповнити пробілами рядки
-до 16 символів. Шифрувати тексти таким чином, щоб кожний символ тексту записувався у два
-байти. Два байти мають таку структуру:
-у бітах 0-3 знаходиться номер рядка символу (4 біти),
-у бітах 4-7 молодша частина ASCII - коду символу (4 біти),
-8 біт – біт парності перших двох полів (1 біт)
-у бітах 9-12 старша частина ASCII - коду символу (4 біти),
-у бітах 13-14 позиція символу в рядку (2 біти),
-15 біт - біт парності попередніх двох полів (1 біт). */
-
 #include <iostream>
-#include <fstream>
 #include <cstring>
 #include <iomanip>
 
 using namespace std;
 
 struct TextCode {
-    unsigned short schkb : 4;   // молодша частина ASCII
-    unsigned short posrow : 4;  // номер рядка
-    unsigned short bitp : 1;     // біт парності
-    unsigned short mchkb : 4;    // старша частина ASCII
-    unsigned short poscol : 4;   // позиція символу в рядку
+    unsigned short schkb : 4;   // Молодший нібл ASCII
+    unsigned short posrow : 4;  // Номер рядка (0-15)
+    unsigned short bitp : 1;    // Біт парності (0/1)
+    unsigned short mchkb : 4;   // Старший нібл ASCII
+    unsigned short poscol : 2;  // Номер колонки (0-3)
 };
 
+static_assert(sizeof(TextCode) == 2, "TextCode має бути 2 байти!");
+
+static unsigned char pbit(unsigned char c) {
+    unsigned char parity = 0;
+    for (int j = 0; j < 8; j++) {
+        if (c & (1 << j)) {
+            parity ^= 1;
+        }
+    }
+    return parity;
+}
+
+static void MyEncryption(char InS[16][5], TextCode OutCoding[64]) {
+    for (int row = 0; row < 16; row++) {
+        for (int col = 0; col < 4; col++) {
+            char c = InS[row][col];
+            if (c == '\0') c = ' ';
+
+            OutCoding[row * 4 + col].schkb = c & 0x0F;
+            OutCoding[row * 4 + col].mchkb = (c >> 4) & 0x0F;
+
+            OutCoding[row * 4 + col].posrow = row;
+            OutCoding[row * 4 + col].poscol = col;
+
+            unsigned char pos = row * 4 + col;
+            OutCoding[row * 4 + col].bitp = pbit(c) ^ pbit(pos);
+        }
+    }
+}
 
 int main() {
-    char S[16][5]; 
+    char S[16][5] = { 0 };
     TextCode Rez[64];
 
-    
-    ifstream ifs("tsk.txt");
-    if (!ifs) {
-        cout << "Файл tsk.txt не відкритий" << endl;
-        return 1;
-    }
-
-    
+    cout << "Введіть 16 рядків тексту (максимум 4 символи в рядку):\n";
     for (int i = 0; i < 16; i++) {
-        ifs.getline(S[i], 5); 
-        
-        for (int j = strlen(S[i]); j < 4; j++) {
+        cin.getline(S[i], 5);
+        int len = strlen(S[i]);
+        for (int j = len; j < 4; j++) {
             S[i][j] = ' ';
         }
-        S[i][4] = '\0'; 
+        S[i][4] = '\0';
     }
-    ifs.close(); 
 
-    // Виклик функції шифрування
+    MyEncryption(S, Rez);
 
-
-    
-    cout << "Зашифровані дані:" << endl;
+    cout << "\nЗашифровані дані:\n";
     for (int i = 0; i < 64; i++) {
-        char c = (Rez[i].mchkb << 4) | Rez[i].schkb; 
-        cout << "Рядок: " << Rez[i].posrow << ", Позиція: " << Rez[i].poscol
-            << ", Символ: '" << c << "', Біт парності: " << Rez[i].bitp << endl;
+        unsigned short encoded = (Rez[i].mchkb << 12) | (Rez[i].bitp << 11) | (Rez[i].posrow << 7) | (Rez[i].poscol << 5) | (Rez[i].schkb);
+        cout << hex << setw(4) << setfill('0') << encoded << endl;
     }
-
-    
-    ofstream ofsb("outbs.bin", ios::out | ios::binary);
-    if (!ofsb) {
-        cout << "Файл outbs.bin не відкритий" << endl;
-        return 1;
-    }
-    ofsb.write((char*)Rez, 64 * sizeof(TextCode));
-    ofsb.close();
-    cout << "Дані записані у outbs.bin" << endl;
 
     return 0;
 }
