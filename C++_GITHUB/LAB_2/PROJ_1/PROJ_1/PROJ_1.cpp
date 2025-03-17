2db5676b49a650f83d5208a1d2ba1bf8e20d04ad
Taras <sleeptaras@gmail.com>
2025-03-18 00:46:31 +0200

    task1 - Дороблення побітової операції виведення звичайної

--- C++_GITHUB/LAB_2/PROJ_1/PROJ_1/PROJ_1.cpp
+++ C++_GITHUB/LAB_2/PROJ_1/PROJ_1/PROJ_1.cpp
@@ -10,11 +10,16 @@ int main()
 
 	// (18*a + 312*d)/512 - b * 122 + c * 123
 
-	int resultat;
+	int resultat1, resultat2;
 
-	resultat = ((a + a +  (a<< 4) + (8 << d) + (6 << d) + d + d) >> 9) - (b << 7) - b - b + (c << 7) - c;
+	resultat1 = (((a << 4) + (a << 1) + (d << 8) + (d << 5) + (d << 3)) >> 9)- ((b << 7) - (b << 2) - (b << 1)) + ((c << 7) - (c << 2) - c);
+	resultat2 = (18 * a + 312 * d) / 512 - b * 122 + c * 123;
 
-	std::cout << "Resultat = " << resultat << std::endl;
+
+
+
+	std::cout << "Resultat = " << resultat1 << std::endl;
+	std::cout << "Resultat = " << resultat2 << std::endl;
 
 
 	return 0;
